5fbeaf17fd9b531001870fd00b4896b3871713c3
Jebb Dykstra <jebb.dykstra@gmail.com>
2026-08-23 21:09:53 +0000

    Share: temp fall back to Cloud Function OG until p.* SSL connects

--- factory.js
+++ factory.js
@@ -108,8 +108,7 @@
   var shareSheetPostId = null;
 
   function postPermalink(postId) {
-    var host = (location.hostname || '').replace(/^www\./i, '') || 'samochat.com';
-    return 'https://p.' + host + '/status/' + encodeURIComponent(String(postId || ''));
+    return 'https://us-central1-subx-skins.cloudfunctions.net/og?site=' + encodeURIComponent(SITE_ID) + '&p=' + encodeURIComponent(String(postId || ''));
   }
 
   function shareTextSlice(post) {
